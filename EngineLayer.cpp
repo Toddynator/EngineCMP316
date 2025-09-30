eef1a0ad820e0102eaab7fa92f2da8f7bd5d4847
Toddynator <Toddynator@hotmail.co.uk>
2025-09-30 01:15:19 +0100

    Implemented the required jolt clases TODO move to physicsManager

--- EngineLayer.cpp
+++ EngineLayer.cpp
@@ -207,7 +207,7 @@ void EngineLayer::Update()
 		// If you have your own custom shape types you probably need to register their handlers with the CollisionDispatch before calling this function.
 		// If you implement your own default material (PhysicsMaterial::sDefault) make sure to initialize it before this function or else this function will create one for you.
 		JPH::RegisterTypes();
-		/*
+		
 		// We need a temp allocator for temporary allocations during the physics update. We're
 		// pre-allocating 10 MB to avoid having to do allocations during the physics update.
 		// B.t.w. 10 MB is way too much for this example but it is a typical value you can use.
@@ -220,7 +220,42 @@ void EngineLayer::Update()
 		// of your own job scheduler. JobSystemThreadPool is an example implementation.
 		JPH::JobSystemThreadPool job_system(2048, 8, thread::hardware_concurrency() - 1);
 
+		// This is the max amount of rigid bodies that you can add to the physics system. If you try to add more you'll get an error.
+	// Note: This value is low because this is a simple test. For a real project use something in the order of 65536.
+		const JPH::uint cMaxBodies = 1024;
+
+		// This determines how many mutexes to allocate to protect rigid bodies from concurrent access. Set it to 0 for the default settings.
+		const JPH::uint cNumBodyMutexes = 0;
+
+		// This is the max amount of body pairs that can be queued at any time (the broad phase will detect overlapping
+		// body pairs based on their bounding boxes and will insert them into a queue for the narrowphase). If you make this buffer
+		// too small the queue will fill up and the broad phase jobs will start to do narrow phase work. This is slightly less efficient.
+		// Note: This value is low because this is a simple test. For a real project use something in the order of 65536.
+		const JPH::uint cMaxBodyPairs = 1024;
+
+		// This is the maximum size of the contact constraint buffer. If more contacts (collisions between bodies) are detected than this
+		// number then these contacts will be ignored and bodies will start interpenetrating / fall through the world.
+		// Note: This value is low because this is a simple test. For a real project use something in the order of 10240.
+		const JPH::uint cMaxContactConstraints = 1024;
+
+		// Create mapping table from object layer to broadphase layer
+		// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
+		// Also have a look at BroadPhaseLayerInterfaceTable or BroadPhaseLayerInterfaceMask for a simpler interface.
+		BPLayerInterfaceImpl broad_phase_layer_interface; // NOTE:: I HAVE TO IMPLEMENT THIS, NOT JOLT
+
+		// Create class that filters object vs broadphase layers
+		// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
+		// Also have a look at ObjectVsBroadPhaseLayerFilterTable or ObjectVsBroadPhaseLayerFilterMask for a simpler interface.
+		ObjectVsBroadPhaseLayerFilterImpl object_vs_broadphase_layer_filter; // NOTE:: I HAVE TO IMPLEMENT THIS, NOT JOLT
+
+		// Create class that filters object vs object layers
+		// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
+		// Also have a look at ObjectLayerPairFilterTable or ObjectLayerPairFilterMask for a simpler interface.
+		ObjectLayerPairFilterImpl object_vs_object_layer_filter; // NOTE:: I HAVE TO IMPLEMENT THIS, NOT JOLT
+
 		JPH::PhysicsSystem physics_system;
+		physics_system.Init(1024, 0, 1024, 1024, broad_phase_layer_interface, object_vs_broadphase_layer_filter, object_vs_object_layer_filter);
+		
 		// The main way to interact with the bodies in the physics system is through the body interface. There is a locking and a non-locking
 		// variant of this. We're going to use the locking version (even though we're not planning to access bodies from multiple threads)
 		JPH::BodyInterface& body_interface = physics_system.GetBodyInterface();
@@ -234,6 +269,7 @@ void EngineLayer::Update()
 		// (note that if we had used CreateBody then we could have set the velocity straight on the body before adding it to the physics system)
 		body_interface.SetLinearVelocity(sphere_id, JPH::Vec3(0.0f, -5.0f, 0.0f));
 
+		/*
 		JPH::uint step = 0;
 		while (body_interface.IsActive(sphere_id))
 		{
@@ -251,13 +287,14 @@ void EngineLayer::Update()
 			// Step the world
 			physics_system.Update(timeManager->getDeltaTime(), cCollisionSteps, &temp_allocator, &job_system);
 		}
+		*/
 
 		// Remove the sphere from the physics system. Note that the sphere itself keeps all of its state and can be re-added at any time.
 		body_interface.RemoveBody(sphere_id);
 
 		// Destroy the sphere. After this the sphere ID is no longer valid.
 		body_interface.DestroyBody(sphere_id);
-		*/
+		
 		// Unregisters all types with the factory and cleans up the default material
 		JPH::UnregisterTypes();
 
