b8d8d58c78cee1da734a192313d4b4c27edce57c
Toddynator <Toddynator@hotmail.co.uk>
2025-10-01 23:14:08 +0100

    logs folder

--- Engine/Graphics/Shader.cpp
+++ Engine/Graphics/Shader.cpp
@@ -308,7 +308,7 @@ void Shader::OutputShaderErrorMessage(ID3D10Blob* errorMessage, HWND hwnd, WCHAR
 	bufferSize = errorMessage->GetBufferSize();
 
 	// Open a file to write the error message to.
-	fout.open("shader-error.txt");
+	fout.open("logs/shader-error.txt");
 
 	// Write out the error message.
 	for (i = 0; i < bufferSize; i++)
